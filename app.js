783361f044468a9bb4f96bf346203167abfbc70f
Manikandan Prakash <140643626+M95kandan@users.noreply.github.com>
2024-03-27 17:22:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 80;  // Change the port to 1000
 
 app.get('/', (req, res) => {
-  res.send('H! team this is a simple Node.js app for testing docker v3...');
+  res.send('H! team this is a simple Node.js app for testing docker v4...');
 });
 
 app.listen(port, '0.0.0.0', () => {
